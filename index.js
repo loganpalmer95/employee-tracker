1d8943ff4c17113462623fd8d317264173d2522d
Logan Palmer <loganpalmer95@icloud.com>
2022-03-14 18:05:05 -0400

    added functionality to index

--- index.js
+++ index.js
@@ -60,3 +60,121 @@ function firstPrompt() {
 		}
 	});
 }
+
+function viewEmployee() {
+	console.log("Employee Rota:\n");
+
+	var query = `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+  FROM employee e
+  LEFT JOIN role r
+	ON e.role_id = r.id
+  LEFT JOIN department d
+  ON d.id = r.department_id
+  LEFT JOIN employee m
+	ON m.id = e.manager_id`;
+
+	connection.query(query, function (err, res) {
+		if (err) throw err;
+
+		console.table(res);
+
+		firstPrompt();
+	});
+}
+
+function viewEmployeeByManager() {
+	console.log("Manager Rota:\n");
+
+	var query = `SELECT e.manager_id, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employee e LEFT JOIN role r
+	ON e.role_id = r.id
+  	LEFT JOIN department d
+  	ON d.id = r.department_id
+  	LEFT JOIN employee m
+	ON m.id = e.manager_id GROUP BY e.manager_id`;
+
+	connection.query(query, function (err, res) {
+		if (err) throw err;
+
+		const managerChoices = res
+			.filter((mgr) => mgr.manager_id)
+			.map(({ manager_id, manager }) => ({
+				value: manager_id,
+				name: manager,
+			}));
+
+		inquirer
+			.prompt(prompt.viewManagerPrompt(managerChoices))
+			.then(function (answer) {
+				var query = `SELECT e.id, e.first_name, e.last_name, r.title, CONCAT(m.first_name, ' ', m.last_name) AS manager
+			FROM employee e
+			JOIN role r
+			ON e.role_id = r.id
+			JOIN department d
+			ON d.id = r.department_id
+			LEFT JOIN employee m
+			ON m.id = e.manager_id
+			WHERE m.id = ?`;
+
+				connection.query(query, answer.managerId, function (err, res) {
+					if (err) throw err;
+
+					console.table("\nManager's subordinates:", res);
+
+					firstPrompt();
+				});
+			});
+	});
+}
+
+function viewEmployeeByDepartment() {
+	console.log("View employees by department\n");
+
+	var query = `SELECT d.id, d.name
+	FROM employee e
+	LEFT JOIN role r
+	ON e.role_id = r.id
+	LEFT JOIN department d
+	ON d.id = r.department_id
+	GROUP BY d.id, d.name`;
+
+	connection.query(query, function (err, res) {
+		if (err) throw err;
+
+		const departmentChoices = res.map((data) => ({
+			value: data.id,
+			name: data.name,
+		}));
+
+		inquirer
+			.prompt(prompt.departmentPrompt(departmentChoices))
+			.then(function (answer) {
+				var query = `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department 
+			FROM employee e
+			JOIN role r
+				ON e.role_id = r.id
+			JOIN department d
+			ON d.id = r.department_id
+			WHERE d.id = ?`;
+
+				connection.query(query, answer.departmentId, function (err, res) {
+					if (err) throw err;
+
+					console.table("\nDepartment Rota: ", res);
+
+					firstPrompt();
+				});
+			});
+	});
+}
+
+function viewDepartments() {
+	var query = "SELECT * FROM department";
+	connection.query(query, function (err, res) {
+		if (err) throw err;
+		console.log(`\nDEPARTMENTS:\n`);
+		res.forEach((department) => {
+			console.log(`ID: ${department.id} | ${department.name} Department`);
+		});
+		firstPrompt();
+	});
+}
